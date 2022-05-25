e24412725fdec5e63f72bf9437f3c21b2f6c6def
jst-r <58705474+jst-r@users.noreply.github.com>
2022-05-25 15:54:57 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 import setuptools
 
-setuptools.setup()
+setuptools.setup(
+    name='calculator_bot',
+    entry_points={
+        'console_scripts': [
+            'serve_bot = tg_bot.serve:main',
+        ],
+    }
+)
