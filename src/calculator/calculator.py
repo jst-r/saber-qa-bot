3a5568bba22632ea7bcd12b7d5c4f3ed1dab405a
jst-r <58705474+jst-r@users.noreply.github.com>
2022-05-25 15:02:02 +0300

    implement repl

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,7 +1,7 @@
 from typing import Iterable
 
-from .constants import OPERATORS
-from .parser import infix_to_postfix, tokenize
+from src.calculator.constants import OPERATORS, OPERATORS_SET
+from src.calculator.parser import infix_to_postfix, tokenize
 
 
 def eval_RPN(rpn: Iterable[str]):
@@ -25,3 +25,24 @@ def eval_str(expr: str):
     tokens = tokenize(expr)
     rpn = infix_to_postfix(tokens)
     return eval_RPN(rpn)
+
+def repr():
+    print("Hello! This is a simple RPN calculator.")
+    print(f"You can only input integers and following operations: {' '.join(OPERATORS_SET)}.")
+    print("To exit the program type q")
+    print("For example 1 + 2 * 3 + 1 / 2")
+
+    while True:
+        expr = input("Type your expression:\n")
+        if expr.lower() == "q":
+            break
+        try:
+            res = eval_str(expr)
+            print(f"{expr} = {res}")
+        except:
+            print("Something went wrong")
+    
+    print("Bye!")
+
+if __name__ == "__main__":
+    repr()
