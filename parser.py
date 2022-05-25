4510c50f1af8e2e1818dc1cccef4b990828d19ae
jst-r <58705474+jst-r@users.noreply.github.com>
2022-05-25 13:58:01 +0300

    implement parsing expression to postfix notation

---
+++ parser.py
@@ -0,0 +1,41 @@
+import re
+from typing import Iterable, List, Union
+
+OPERATORS = set(['+', '-', '*', '/', '^'])
+PRECEDENCE = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
+
+
+def tokenize(expr: str) -> List[int]:
+    '''
+    Splits input string into tokens.
+    Token is a number or an operator.
+    '''
+    pattern = re.compile(r"([0-9]+|[\+\-\*/\^])")
+    return re.findall(pattern, expr)
+
+
+def infix_to_postfix(infix: Iterable[str]):
+    stack = []
+    postfix = []
+
+    for token in infix:
+        if token.isnumeric():
+            postfix.append(token)
+            continue
+
+        while stack and PRECEDENCE[token] <= PRECEDENCE[stack[-1]]:
+            if token == stack[-1] == '^':
+                break
+
+            postfix.append(stack.pop())
+        
+        stack.append(token)
+
+    while stack:
+        postfix.append(stack.pop())
+    
+    return postfix
+
+print(infix_to_postfix(tokenize("1 + 2 + 3 * 3")))
+print(tokenize("1+2+3"))
+print(tokenize("123 + 2 "))
